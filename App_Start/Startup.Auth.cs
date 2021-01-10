2927bf47a6c608a2bfb2fc75fd3e67438797ca07
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2021-01-11 00:23:19 +0200

    Updates

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -14,6 +14,8 @@ namespace CarMessenger
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            app.MapSignalR();
+
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
