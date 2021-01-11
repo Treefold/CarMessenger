864a3d29ab87f367db0fbe65706fa06c454634d6
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2021-01-11 20:32:50 +0200

    NoDBMesseger

--- ChatHub.cs
+++ ChatHub.cs
@@ -6,10 +6,22 @@ namespace CarMessenger
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send (string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
+        }
+        private string CarGroup (string carPlate, string carCountryCode)
+        {
+            return carPlate + "_" + carCountryCode;
+        }
+        public void JoinCar (string carPlate, string carCountryCode)
+        {
+            Groups.Add(Context.ConnectionId, CarGroup(carPlate, carCountryCode));
+        }
+        public void MessageCar(string mail, string nickname, string carPlate, string carCountryCode, string message)
+        {
+            Clients.Group(CarGroup(carPlate, carCountryCode)).addMessage(nickname, message);
         }
     }
 }
