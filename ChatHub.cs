7274058c74a83aa3bf1b91c9c35dd0d5e757d8ee
Mihai Daniel Surcea <mihaidanielsurcea@gmail.com>
2021-01-11 16:09:06 +0200

    NonLoginMultiChat

--- ChatHub.cs
+++ ChatHub.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.AspNet.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Web;
+using Microsoft.AspNet.SignalR;
 
 namespace CarMessenger
 {
     public class ChatHub : Hub
     {
-        public void Hello()
-        {
-            Clients.All.hello();
-        }
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
